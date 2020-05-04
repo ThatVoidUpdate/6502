9949a2fd1bddabe9a6b24b97fa0d02b8ba1200c7
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-05-04 13:53:15 +0100

    Changed romWriter to make it easier to write code

--- romWriter.py
+++ romWriter.py
@@ -6,16 +6,11 @@ with open("rom.bin", "wb") as output:
         
         rom.append(bytes((0xea,)))
 
-    rom[0x0] = bytes((0xa9,))
-    rom[0x1] = bytes((0x80,))
+    program = [0xa9, 0x80, 0x85, 0x01, 0x65, 0x01, 0x00]
+    
+    print(bytes(program))
 
-    rom[0x2] = bytes((0x85,))
-    rom[0x3] = bytes((0x01,))
-
-    rom[0x4] = bytes((0x65,))
-    rom[0x5] = bytes((0x01,))
-
-    rom[0x6] = bytes((0x00,))
+    rom[0:len(program)] = [bytes((x,)) for x in program]
 
     rom[0xfffc] = bytes((0x00,))
     rom[0xfffd] = bytes((0x00,))
